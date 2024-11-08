e657e5900ea22642fc161ee7dda3d6e9d892d5fa
OlenaX2024 <olenax2024@gmail.com>
2024-11-09 00:45:31 +0400

    add all file

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const categories = document.querySelectorAll('#categories .item');
 console.log('Number of categories: ', categories.length); // Number of categories:  3
 categories.forEach(category => {
-  const categoryElementsCount = category.querySelectorAll('ul li');
+  const categoryElementsCount = category.querySelectorAll('ul, li');
   for (let i = 0; i < categoryElementsCount.length; i++) {
     categoryElementsCount[i].classList.add('option-item');
   }
